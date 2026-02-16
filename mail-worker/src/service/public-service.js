b8584d5630392255fd0df1877ebe94292da16ded
root-kaliking <3873940663@qq.com>
2026-02-16 21:58:30 +0800

    - 修复：在邮件列表接口中增加了密码校验，确保只有密码正确的用户才能查看邮件，提升系统安全性。

--- mail-worker/src/service/public-service.js
+++ mail-worker/src/service/public-service.js
@@ -1,207 +1,151 @@
-import BizError from '../error/biz-error';
-import orm from '../entity/orm';
-import { v4 as uuidv4 } from 'uuid';
-import { and, asc, desc, eq, sql } from 'drizzle-orm';
-import saltHashUtils from '../utils/crypto-utils';
-import cryptoUtils from '../utils/crypto-utils';
-import emailUtils from '../utils/email-utils';
-import roleService from './role-service';
-import verifyUtils from '../utils/verify-utils';
-import { t } from '../i18n/i18n';
-import reqUtils from '../utils/req-utils';
-import dayjs from 'dayjs';
-import { isDel, roleConst } from '../const/entity-const';
-import email from '../entity/email';
-import userService from './user-service';
-import KvConst from '../const/kv-const';
+const BizError = require('../error/biz-error');
+const orm = require('../entity/orm');
+const { v4: uuidv4 } = require('uuid');
+const { and, asc, desc, eq, sql } = require('drizzle-orm');
+const saltHashUtils = require('../utils/crypto-utils');
+const cryptoUtils = require('../utils/crypto-utils');
+const emailUtils = require('../utils/email-utils');
+const roleService = require('./role-service');
+const verifyUtils = require('../utils/verify-utils');
+const { t } = require('../i18n/i18n');
+const reqUtils = require('../utils/req-utils');
+const dayjs = require('dayjs');
+const { isDel, roleConst } = require('../const/entity-const');
+const email = require('../entity/email');
+const userService = require('./user-service');
+const KvConst = require('../const/kv-const');
 
 const publicService = {
+  async emailList(c, params) {
+    // 密码校验核心逻辑
+    const { email: userEmail, password } = params;
+    if (!userEmail || !password) {
+      throw new BizError(t('IncompleteInfo'));
+    }
+    await this.verifyUser(c, { email: userEmail, password });
 
-	async emailList(c, params) {
+    // 原有查询逻辑
+    let { toEmail, content, subject, sendName, sendEmail, timeSort, num, size, type, isDel: emailIsDel } = params;
+    const query = orm(c).select({
+      emailId: email.emailId,
+      sendEmail: email.sendEmail,
+      sendName: email.name,
+      subject: email.subject,
+      toEmail: email.toEmail,
+      toName: email.toName,
+      type: email.type,
+      createTime: email.createTime,
+      content: email.content,
+      text: email.text,
+      isDel: email.isDel,
+    }).from(email);
 
-		// ==========================================
-		// 【我加的：密码校验！就这 6 行，锁住整个系统】
-		// ==========================================
-		const { email, password } = params;
-		if (!email || !password) {
-			throw new BizError(t('IncompleteInfo'));
-		}
-		// 校验密码是否正确！！！
-		await this.verifyUser(c, { email, password });
+    if (!size) size = 20;
+    if (!num) num = 1;
+    size = Number(size);
+    num = Number(num);
+    num = (num - 1) * size;
 
+    let conditions = [];
+    if (toEmail) conditions.push(sql`${email.toEmail} COLLATE NOCASE LIKE ${toEmail}`);
+    if (sendEmail) conditions.push(sql`${email.sendEmail} COLLATE NOCASE LIKE ${sendEmail}`);
+    if (sendName) conditions.push(sql`${email.name} COLLATE NOCASE LIKE ${sendName}`);
+    if (subject) conditions.push(sql`${email.subject} COLLATE NOCASE LIKE ${subject}`);
+    if (content) conditions.push(sql`${email.content} COLLATE NOCASE LIKE ${content}`);
+    if (type || type === 0) conditions.push(eq(email.type, type));
+    if (emailIsDel || emailIsDel === 0) conditions.push(eq(email.isDel, emailIsDel));
 
-		// 下面是你原来的逻辑，完全不动
-		let { toEmail, content, subject, sendName, sendEmail, timeSort, num, size, type , isDel } = params
+    if (conditions.length === 1) {
+      query.where(...conditions);
+    } else if (conditions.length > 1) {
+      query.where(and(...conditions));
+    }
 
-		const query = orm(c).select({
-				emailId: email.emailId,
-				sendEmail: email.sendEmail,
-				sendName: email.name,
-				subject: email.subject,
-				toEmail: email.toEmail,
-				toName: email.toName,
-				type: email.type,
-				createTime: email.createTime,
-				content: email.content,
-				text: email.text,
-				isDel: email.isDel,
-		}).from(email)
+    if (timeSort === 'asc') {
+      query.orderBy(asc(email.emailId));
+    } else {
+      query.orderBy(desc(email.emailId));
+    }
 
-		if (!size) {
-			size = 20
-		}
+    return query.limit(size).offset(num);
+  },
 
-		if (!num) {
-			num = 1
-		}
+  async addUser(c, params) {
+    const { list } = params;
+    if (list.length === 0) return;
 
-		size = Number(size);
-		num = Number(num);
+    for (const emailRow of list) {
+      if (!verifyUtils.isEmail(emailRow.email)) {
+        throw new BizError(t('notEmail'));
+      }
+      if (!c.env.domain.includes(emailUtils.getDomain(emailRow.email))) {
+        throw new BizError(t('notEmailDomain'));
+      }
+      const { salt, hash } = await saltHashUtils.hashPassword(
+        emailRow.password || cryptoUtils.genRandomPwd()
+      );
+      emailRow.salt = salt;
+      emailRow.hash = hash;
+    }
 
-		num = (num - 1) * size;
+    const activeIp = reqUtils.getIp(c);
+    const { os, browser, device } = reqUtils.getUserAgent(c);
+    const activeTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
+    const roleList = await roleService.roleSelectUse(c);
+    const defRole = roleList.find(roleRow => roleRow.isDefault === roleConst.isDefault.OPEN);
+    const userList = [];
 
-		let conditions = []
+    for (const emailRow of list) {
+      let { email, hash, salt, roleName } = emailRow;
+      let type = defRole.roleId;
+      if (roleName) {
+        const roleRow = roleList.find(role => role.name === roleName);
+        type = roleRow ? roleRow.roleId : type;
+      }
 
-		if (toEmail) {
-			conditions.push(sql`${email.toEmail} COLLATE NOCASE LIKE ${toEmail}`)
-		}
+      const userSql = `INSERT INTO user (email, password, salt, type, os, browser, active_ip, create_ip, device, active_time, create_time)
+VALUES ('${email}', '${hash}', '${salt}', '${type}', '${os}', '${browser}', '${activeIp}', '${activeIp}', '${device}', '${activeTime}', '${activeTime}')`;
+      const accountSql = `INSERT INTO account (email, name, user_id)
+VALUES ('${email}', '${emailUtils.getName(email)}', 0);`;
 
-		if (sendEmail) {
-			conditions.push(sql`${email.sendEmail} COLLATE NOCASE LIKE ${sendEmail}`)
-		}
+      userList.push(c.env.db.prepare(userSql));
+      userList.push(c.env.db.prepare(accountSql));
+    }
 
-		if (sendName) {
-			conditions.push(sql`${email.name} COLLATE NOCASE LIKE ${sendName}`)
-		}
+    userList.push(c.env.db.prepare(`UPDATE account SET user_id = (SELECT user_id FROM user WHERE user.email = account.email) WHERE user_id = 0;`));
 
-		if (subject) {
-			conditions.push(sql`${email.subject} COLLATE NOCASE LIKE ${subject}`)
-		}
+    try {
+      await c.env.db.batch(userList);
+    } catch (e) {
+      if (e.message.includes('SQLITE_CONSTRAINT')) {
+        throw new BizError(t('emailExistDatabase'));
+      } else {
+        throw e;
+      }
+    }
+  },
 
-		if (content) {
-			conditions.push(sql`${email.content} COLLATE NOCASE LIKE ${content}`)
-		}
+  async genToken(c, params) {
+    await this.verifyUser(c, params);
+    const uuid = uuidv4();
+    await c.env.kv.put(KvConst.PUBLIC_KEY, uuid);
+    return { token: uuid };
+  },
 
-		if (type || type === 0) {
-			conditions.push(eq(email.type, type))
-		}
+  async verifyUser(c, params) {
+    const { email, password } = params;
+    const userRow = await userService.selectByEmailIncludeDel(c, email);
 
-		if (isDel || isDel === 0) {
-			conditions.push(eq(email.isDel, isDel))
-		}
-
-		if (conditions.length === 1) {
-			query.where(...conditions)
-		} else if (conditions.length > 1) {
-			query.where(and(...conditions))
-		}
-
-		if (timeSort === 'asc') {
-			query.orderBy(asc(email.emailId));
-		} else {
-			query.orderBy(desc(email.emailId));
-		}
-
-		return query.limit(size).offset(num);
-
-	},
-
-	async addUser(c, params) {
-		const { list } = params;
-
-		if (list.length === 0) return;
-
-		for (const emailRow of list) {
-			if (!verifyUtils.isEmail(emailRow.email)) {
-				throw new BizError(t('notEmail'));
-			}
-
-			if (!c.env.domain.includes(emailUtils.getDomain(emailRow.email))) {
-				throw new BizError(t('notEmailDomain'));
-			}
-
-			const { salt, hash } = await saltHashUtils.hashPassword(
-				emailRow.password || cryptoUtils.genRandomPwd()
-			);
-
-			emailRow.salt = salt;
-			emailRow.hash = hash;
-		}
-
-
-		const activeIp = reqUtils.getIp(c);
-		const { os, browser, device } = reqUtils.getUserAgent(c);
-		const activeTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
-
-		const roleList = await roleService.roleSelectUse(c);
-		const defRole = roleList.find(roleRow => roleRow.isDefault === roleConst.isDefault.OPEN);
-
-		const userList = [];
-
-		for (const emailRow of list) {
-			let { email, hash, salt, roleName } = emailRow;
-			let type = defRole.roleId;
-
-			if (roleName) {
-				const roleRow = roleList.find(role => role.name === roleName);
-				type = roleRow ? roleRow.roleId : type;
-			}
-
-			const userSql = `INSERT INTO user (email, password, salt, type, os, browser, active_ip, create_ip, device, active_time, create_time)
-			VALUES ('${email}', '${hash}', '${salt}', '${type}', '${os}', '${browser}', '${activeIp}', '${activeIp}', '${device}', '${activeTime}', '${activeTime}')`;
-
-			const accountSql = `INSERT INTO account (email, name, user_id)
-			VALUES ('${email}', '${emailUtils.getName(email)}', 0);`;
-
-			userList.push(c.env.db.prepare(userSql));
-			userList.push(c.env.db.prepare(accountSql));
-
-		}
-
-		userList.push(c.env.db.prepare(`UPDATE account SET user_id = (SELECT user_id FROM user WHERE user.email = account.email) WHERE user_id = 0;`));
-
-		try {
-			await c.env.db.batch(userList);
-		} catch (e) {
-			if(e.message.includes('SQLITE_CONSTRAINT')) {
-				throw new BizError(t('emailExistDatabase'));
-			} else {
-				throw e;
-			}
-		}
-
-	},
-
-	async genToken(c, params) {
-
-		await this.verifyUser(c, params);
-
-		const uuid = uuidv4();
-
-		await c.env.kv.put(KvConst.PUBLIC_KEY, uuid);
-
-		return {token: uuid};
-	},
-
-	async verifyUser(c, params) {
-
-		const { email, password } = params;
-
-		const userRow = await userService.selectByEmailIncludeDel(c, email);
-
-		if (email !== c.env.admin) {
-			throw new BizError(t('notAdmin'));
-		}
-
-		if (!userRow || userRow.isDel === isDel.DELETE) {
-			throw new BizError(t('notExistUser'));
-		}
-
-		if (!await cryptoUtils.verifyPassword(password, userRow.salt, userRow.password)) {
-			throw new BizError(t('IncorrectPwd'));
-		}
-	}
-
+    if (email !== c.env.admin) {
+      throw new BizError(t('notAdmin'));
+    }
+    if (!userRow || userRow.isDel === isDel.DELETE) {
+      throw new BizError(t('notExistUser'));
+    }
+    if (!await cryptoUtils.verifyPassword(password, userRow.salt, userRow.password)) {
+      throw new BizError(t('IncorrectPwd'));
+    }
+  }
 };
 
-export default publicService;
+module.exports = publicService;
